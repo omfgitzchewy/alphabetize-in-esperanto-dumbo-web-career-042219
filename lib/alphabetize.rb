eb79d882b189ba219ef0975540ce2bdce89c6ec6
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 19:09:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
    esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   array.sort_by do |string|
-    string.split("").map do |letter|
+    string.split("").collect do |letter|
       esperanto.index(letter)
     end
   end
