7adcab6f9c0936d2845a960f05fbc2bcceaa3be5
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 18:53:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,10 @@
 def alphabetize(array)
-  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  array.sort_by do |word|
-    word.split("").collect do |letter|
-      esperanto.index(letter)
-    end
-  end
+  # esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  # array.sort_by do |word|
+  #   word.split("").collect do |letter|
+  #     esperanto.index(letter)
+  #   end
+  # end
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(esp_alph, ascii)}
 end
