0f5d4e9ab36492bb18562269dc5657862e522cd5
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 19:08:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -13,6 +13,5 @@ def alphabetize(arr)
   esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by {|string|
     [esp_alph.index(string[0]),
-     esp_alph.index(string[1]),
-     esp_alph.index(string[2])]}
+     esp_alph.index(string[1])]}
 end
