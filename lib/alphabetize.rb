172c0c952420dad77303ccc514bdc40cda48bc75
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 18:46:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,9 @@
 def alphabetize(arr)
   letters = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
+    word.split('').collect do |letter|
+      alphabet.index(letter)
+    end
 
   end
 end
