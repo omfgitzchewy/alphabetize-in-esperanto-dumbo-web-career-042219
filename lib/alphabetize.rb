d3e1cb1e3227c9204f25d92e8e3b0b5026d9c620
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 19:12:02 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(array)
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  array.sort_by do |string|
+    string.split("").map do |letter|
+      esperanto.index(letter)
+    end
+  end
+
+  # figuring this code out...
+  # ascii = "@!ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  # array.sort_by{|string| string.tr(esperanto, ascii)}
+
 end
